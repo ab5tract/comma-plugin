8cd2002ba6c916948b27a84a4de9a2d6cdd7963f
ab5tract <longwalker@hey.com>
2024-10-08 19:17:08 +0200

    Only special-case <.ws> when not declared in scope

--- src/main/java/org/raku/comma/psi/impl/RakuRegexAssertionImpl.java
+++ src/main/java/org/raku/comma/psi/impl/RakuRegexAssertionImpl.java
@@ -9,8 +9,10 @@ import org.raku.comma.parsing.RakuTokenTypes;
 import org.raku.comma.psi.RakuRegexAssertion;
 import org.raku.comma.psi.RakuRegexCall;
 import org.jetbrains.annotations.NotNull;
+import org.raku.comma.psi.RakuRegexDecl;
 
 import java.util.Collection;
+import java.util.List;
 
 public class RakuRegexAssertionImpl extends RakuASTWrapperPsiElement implements RakuRegexAssertion {
     public RakuRegexAssertionImpl(@NotNull ASTNode node) {
@@ -20,12 +22,13 @@ public class RakuRegexAssertionImpl extends RakuASTWrapperPsiElement implements 
     @Override
     public String getName() {
         Collection<RakuRegexCall> calls = PsiTreeUtil.findChildrenOfType(this, RakuRegexCall.class);
-        if (calls.size() == 1)
+        if (calls.size() == 1) {
             return "$" + getText();
-        else if (!calls.isEmpty()) {
+        } else if (!calls.isEmpty()) {
             RakuRegexCall next = calls.iterator().next();
-            if (next != null)
+            if (next != null) {
                 return "$<" + next.getText() + ">";
+            }
         }
         return null;
     }
@@ -38,14 +41,28 @@ public class RakuRegexAssertionImpl extends RakuASTWrapperPsiElement implements 
     @Override
     public boolean mightMatchZeroWidth() {
         // Anything with ? or ! will be zero-width.
-        if (getNode().findChildByType(RakuTokenTypes.REGEX_LOOKAROUND) != null)
+        if (getNode().findChildByType(RakuTokenTypes.REGEX_LOOKAROUND) != null) {
             return true;
+        }
 
-        // Special-case the ws rule.
+        // Special-case the ws rule. But only when there is not a custom ws declared in scope.
         RakuRegexCall call = PsiTreeUtil.getChildOfType(this, RakuRegexCall.class);
+
         if (call != null) {
+            boolean wsDeclaredInScope = false;
+            List<RakuRegexDecl> decls = PsiTreeUtil.collectElementsOfType(this.getContainingFile(), RakuRegexDecl.class).stream().toList();
+
+            if (! decls.isEmpty()) {
+                for (RakuRegexDecl decl : decls) {
+                    if (decl.getRegexName().equals("ws")) {
+                        wsDeclaredInScope = true;
+                        break;
+                    }
+                }
+            }
+
             String name = call.getName();
-            return name != null && name.equals("ws");
+            return !wsDeclaredInScope && name != null && name.equals("ws");
         }
 
         return false;
