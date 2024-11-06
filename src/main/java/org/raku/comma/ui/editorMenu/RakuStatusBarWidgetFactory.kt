bbfd00b3c5d5215a2454ca845f14d0cdba4c4302
ab5tract <longwalker@hey.com>
2024-11-06 15:21:33 -0500

    Add '🦋' menu
    This allows for quickly shifting between SDKs and launching a REPL.
    
    We can also add future actions here.

--- src/main/java/org/raku/comma/ui/editorMenu/RakuStatusBarWidgetFactory.kt
+++ src/main/java/org/raku/comma/ui/editorMenu/RakuStatusBarWidgetFactory.kt
@@ -1,15 +1,21 @@
 package org.raku.comma.ui.editorMenu
 
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.StatusBarWidgetFactory
 import kotlinx.coroutines.CoroutineScope
+import org.raku.comma.services.project.RakuProjectDetailsService
 
 class RakuStatusBarWidgetFactory : StatusBarWidgetFactory {
     private val ID: String = "RakuStatusBarWidgetFactory"
 
     override fun getId(): String {
         return ID
+    }
+
+    override fun isAvailable(project: Project): Boolean {
+        return project.service<RakuProjectDetailsService>().doesProjectContainRakuCode
     }
 
     override fun createWidget(project: Project, scope: CoroutineScope): StatusBarWidget {
